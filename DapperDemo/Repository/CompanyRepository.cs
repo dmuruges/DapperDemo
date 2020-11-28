e02ce2546b54271380a0dc0bc262e8f1ef765ffd
Dhanasekar Murugesan <sekar.murugesan@gmail.com>
2020-11-28 21:38:26 +0530

    Configuring Dapper Repository

---
+++ DapperDemo/Repository/CompanyRepository.cs
@@ -0,0 +1,47 @@
+﻿using DapperDemo.Data;
+using DapperDemo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private IDbConnection db;
+
+        public CompanyRepository(IConfiguration configuration)
+        {
+            this.db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public Company Add(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Company> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Update(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
